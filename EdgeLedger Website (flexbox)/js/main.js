85b9cdda0fcb79133f61d01fc813a159979c215e
Daniel Monroe <danielmonroe00@gmail.com>
2019-01-30 08:45:12 -0800

    Added smooth scrolling and sticky menu styling

--- EdgeLedger Website (flexbox)/js/main.js
+++ EdgeLedger Website (flexbox)/js/main.js
@@ -1,12 +1,3 @@
-var map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 8
-        });
-      }
-
-
 // Initialize and add the map
 function initMap() {
   // Your Location
@@ -20,3 +11,28 @@ function initMap() {
   // The marker, positioned at location
   const marker = new google.maps.Marker({ position: loc, map: map});
 }
+
+// Sticky menu background
+window.addEventListener('scroll', function() {
+  if(window.scrollY > 150) {
+    document.querySelector('#navbar').style.opacity = 0.9;
+  } else {
+    document.querySelector('#navbar').style.opacity = 1;
+  }
+});
+
+// Smooth Scrolling
+$('#navbar a, .btn').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 65
+      },
+      600
+    );
+  }
+});
